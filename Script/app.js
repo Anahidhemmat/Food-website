4c21f055edb44e1653e7e0b3e5e438c0007da459
Anahid <anahidhemmat2130@gmail.com>
2022-03-07 00:24:25 +0400

    search bar js

--- Script/app.js
+++ Script/app.js
@@ -1,9 +1,22 @@
 //selectors
 const menuBar = document.querySelector("#menu-bars");
 const navbar = document.querySelector(".navbar");
+const searchIcon = document.querySelector("#search-icon");
+const searchForm = document.querySelector("#search-form");
+const searchCloseIcon = document.querySelector("#close");
 
 menuBar.addEventListener("click", () => {
   menuBar.classList.toggle("fa-times");
   navbar.classList.toggle("show");
-  console.log(navbar.classList);
+});
+
+window.addEventListener("scroll", () => {
+  menuBar.classList.remove("fa-times");
+  navbar.classList.remove("show");
+});
+searchIcon.addEventListener("click", () => {
+  searchForm.style.top = "0";
+});
+searchCloseIcon.addEventListener("click", () => {
+  searchForm.style.top = "-110%";
 });
